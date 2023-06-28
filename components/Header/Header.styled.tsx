417a72abee943099bad9d1c9848ffd8c9b028313
aeramisant <aeramisant@griddynamics.com>
2023-06-28 11:41:43 +0300

    add balel styled components plugin to solvethe hjydration issiue with styled components

--- components/Header/Header.styled.tsx
+++ components/Header/Header.styled.tsx
@@ -3,6 +3,6 @@ import styled, { css } from 'styled-components';
 export const StyledHeader = styled.div`
   background-color: ${({ theme }) => theme.colors.primary};
   h1 {
-    ${({ theme }) => theme.mixins.primaryHeroRegular};
+    ${({ theme }) => theme.mixins.primaryHeading300};
   }
 `;

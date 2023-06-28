4fa7ba91d1bb03aa15e12845b1444437ada97ce0
Razvan-Bogdan Turiatca <rturiatca@griddynamics.com>
2023-06-28 09:44:31 +0300

    aaa

--- styles/productId.styled.js
+++ styles/productId.styled.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
-export const ProductId = styled.div`
+export const ProductId = styled.section`
   width: 1000px;
-  background-color: pink;
   display: flex;
+  margin: 0 auto;
   .imageContainer {
     padding: 40px;
+    background-color: green;
+  }
+  .productDetails {
+    background-color: blue;
   }
   h5 {
     color: red;

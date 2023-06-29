33f3574eeb8b2244ecc73ba3ac69f3a37bd9a252
Alex Eramisant <eramisant@yahoo.com>
2023-06-29 10:04:25 +0300

    Landing2 (#9)
    * cleaned up Header component folder
    
    * changed opacity in slider
    
    ---------
    
    Co-authored-by: aeramisant <aeramisant@griddynamics.com>

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,6 @@
-import { StyledHeader } from './Header.styled';
-import Burger from './Burger/Burger';
-import SliderNav from './SliderNav/SliderNav';
+import styled, { css } from 'styled-components';
+import Burger from './Burger';
+import SliderNav from './SliderNav';
 import { useState, useEffect } from 'react';
 import ShoppingCartIcon from '../icons/shopping_cart.svg';
 import Image from 'next/image';
@@ -12,11 +12,42 @@ const Header = () => {
   return (
     <StyledHeader>
       <SliderNav isOpen={isOpen} setIsOpen={setIsOpen} />
-      <Burger setIsOpen={setIsOpen} isOpen={isOpen} className="burger-hidden" />
+      <Burger setIsOpen={setIsOpen} isOpen={isOpen} />
       <h3>BSW</h3>
       <Image src={ShoppingCartIcon} alt="shopping-cart-icon" />
     </StyledHeader>
   );
 };
 
+export const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  opacity: 0.5;
+  padding: 18px 64px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  h3 {
+    font-size: 32px;
+    font-family: ${({ theme }) => theme.fonts.secondary};
+    font-weight: 500;
+    letter-spacing: 10px;
+    color: ${({ theme }) => theme.colors.whitePrimary};
+  }
+  .burger-hidden {
+    display: none !important;
+  }
+  @media (max-width: 768px) {
+    .burger-hidden {
+      display: block !important;
+    }
+    .navar-navitems {
+      display: none;
+    }
+    .navbar {
+      margin: 0;
+      padding: 1rem;
+    }
+  }
+`;
 export default Header;

ec29b6f291c32c26958cd92418db5d504f0f3aa0
Alex Eramisant <eramisant@yahoo.com>
2023-07-19 11:47:26 +0300

    fix styled loadingafter html bug (#54)
    Co-authored-by: aeramisant <aeramisant@griddynamics.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//         <div id="notifications"></div>
+//       </body>
+//     </Html>
+//   );
+// }
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+function MyDocument(props: any) {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
         <Main />
@@ -12,3 +33,31 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props: any) =>
+          sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default MyDocument;
